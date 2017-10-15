6cf56c0279ec670fbbfec25a7c42629d55ab0d02
Daniel Blazevski <danielblazevski@dbmacbook.local>
2017-10-15 10:24:47 -0400

    adding time to show runs in much less than 1000 ms

--- src/main/java/futuresExample.java
+++ src/main/java/futuresExample.java
@@ -38,8 +38,13 @@ public class futuresExample {
     }
 
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
         slowConcatFuture scf = new futuresExample().new slowConcatFuture();
          String s = scf.asyncSlowConcat(" olleh", "!dlrow");
          System.out.println(s);
+        long endTime = System.currentTimeMillis();
+        long duration = (endTime - startTime);
+        System.out.println(duration);
+
     }
 }

8e0bb15154776e9f53caec4f32160b4ac75e850e
Daniel Blazevski <danielblazevski@dbmacbook.local>
2017-10-15 09:07:43 -0400

    cleaned up class struct, added pom

--- src/main/java/futuresExample.java
+++ src/main/java/futuresExample.java
@@ -3,36 +3,43 @@ import java.util.concurrent.CompletionStage;
 
 public class futuresExample {
 
-    // helper function that sleeps.  Used to minimize code in slowReverseFuture
-    // can't do Thread.sleep(t) w/o try/catch statement.
-    private static void delay(long t) {
-        try { Thread.sleep(t); } catch (InterruptedException e) {
-            System.out.println(e);}
-    }
+    public class slowConcatFuture {
 
-    // simultaneously reverses str1 and str2
-    // then combines the results and concatenates them
-    private static String asyncSlowConcat(final String str1, final String str2) {
-        CompletionStage<String> reverse1 = slowReverseFuture(str1);
-        CompletionStage<String> reverse2ThenConcat = slowReverseFuture(str2)
-                .thenCombineAsync(reverse1, (s2, s1) -> s1.concat(s2));
+        // helper function that sleeps.  Used to minimize code in slowReverseFuture
+        // can't do Thread.sleep(t) w/o try/catch statement.
+        private void delay(long t) {
+            try {
+                Thread.sleep(t);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
 
-        CompletableFuture comp = reverse2ThenConcat.toCompletableFuture();
-        return comp.join().toString();
-    }
+        // simultaneously reverses str1 and str2
+        // then combines the results and concatenates them
+        public String asyncSlowConcat(final String str1, final String str2) {
+            CompletionStage<String> reverse1 = slowReverseFuture(str1);
+            CompletionStage<String> reverse2ThenConcat = slowReverseFuture(str2)
+                    .thenCombineAsync(reverse1, (s2, s1) -> s1.concat(s2));
 
-    // function that creates a Future out of string w/ the string reversed
-    // a delay is added to make it "slow"
-    private static CompletionStage<String> slowReverseFuture(String str){
-        return CompletableFuture.supplyAsync(() -> {
-                    delay(500);
-                    return new StringBuilder(str).reverse().toString();
-                }
-        );
+            CompletableFuture comp = reverse2ThenConcat.toCompletableFuture();
+            return comp.join().toString();
+        }
+
+        // function that creates a Future out of string w/ the string reversed
+        // a delay is added to make it "slow"
+        private CompletionStage<String> slowReverseFuture(String str) {
+            return CompletableFuture.supplyAsync(() -> {
+                        delay(500);
+                        return new StringBuilder(str).reverse().toString();
+                    }
+            );
+        }
     }
 
     public static void main(String[] args) {
-         String s = asyncSlowConcat(" olleh", "!dlrow");
+        slowConcatFuture scf = new futuresExample().new slowConcatFuture();
+         String s = scf.asyncSlowConcat(" olleh", "!dlrow");
          System.out.println(s);
     }
 }
